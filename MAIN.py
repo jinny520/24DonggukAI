3ae91ccb326460d5086769f4f3e714195810ca82
jinny520 <50129077+jinny520@users.noreply.github.com>
2024-08-15 01:38:22 +0900

    Create MAIN.py

---
+++ MAIN.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("2024 동대부고 공유캠퍼스")
+st.title("인공지능과 피지컬컴퓨팅 자료실 made by은진T")
+
+st.write("아래 버튼을 클릭하면 실습자료가 저장된 GOOGLE DRIVE로 연결됩니다.")
+#하이퍼링크 버튼 생성 
+st.link_button("실습자료","https://drive.google.com/drive/folders/1Ya3eBHfnWkA-IyfVZit8b59KJyMzDhqp?usp=sharing")
