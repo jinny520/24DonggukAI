4ef76838c43f72d7a90cc0c9d6920c970d48a1fd
jinny520 <50129077+jinny520@users.noreply.github.com>
2024-08-23 15:02:26 +0900

    Update 02_LectureNotes.py

--- pages/02_LectureNotes.py
+++ pages/02_LectureNotes.py
@@ -11,7 +11,7 @@ if os.path.exists(pdf_file_path):
     # PDF 파일 다운로드 버튼
     with open(pdf_file_path, "rb") as file:
         btn = st.download_button(
-            label="Download",
+            label="240823 Lecture note Download",
             data=file,
             file_name="day1.pdf",
             mime="application/pdf"  # 여기에 문자열을 제대로 닫았습니다.
