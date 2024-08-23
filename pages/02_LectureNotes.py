1c6f0180be988c674c3c6918e9663b69f92164a3
jinny520 <50129077+jinny520@users.noreply.github.com>
2024-08-23 15:01:15 +0900

    Update 02_LectureNotes.py

--- pages/02_LectureNotes.py
+++ pages/02_LectureNotes.py
@@ -3,9 +3,8 @@ import os
 
 st.title("💕 강의자료 👩‍🦰")
   
-
 # PDF 파일 경로, 현재 스크립트 위치 기준 
-image_path = os.path.join(os.path.dirname(__file__), "day1.pdf")
+pdf_file_path = os.path.join(os.path.dirname(__file__), "day1.pdf")
 
 # 파일 존재 여부 확인
 if os.path.exists(pdf_file_path):
@@ -15,8 +14,4 @@ if os.path.exists(pdf_file_path):
             label="Download",
             data=file,
             file_name="day1.pdf",
-            mime="application/pdf"
-        )
-    st.write("아래 버튼을 클릭하여 PDF 파일을 다운로드하세요.")
-else:
-    st.error("파일을 찾을 수 없습니다. 파일 경로와 이름을 확인해주세요.")
+            mime="application/pdf
