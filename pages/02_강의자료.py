6aad0ed1e3614f75ff34537a65b3f441b435c287
jinny520 <50129077+jinny520@users.noreply.github.com>
2024-09-06 13:05:31 +0900

    Update 02_강의자료.py

--- pages/02_강의자료.py
+++ pages/02_강의자료.py
@@ -12,9 +12,9 @@ if os.path.exists(pdf_file_path_lecture):
     # 강의자료 PDF 파일 다운로드 버튼
     with open(pdf_file_path_lecture, "rb") as file:
         btn_lecture = st.download_button(
-            label="240823 수업PPT Download",
+            label="분류와 회귀 수업자료",
             data=file,
-            file_name="day2.pdf",
+            file_name="002.pdf",
             mime="application/pdf"
         )
     st.write("버튼을 클릭하여 강의자료 PDF 파일을 다운로드하세요.")
