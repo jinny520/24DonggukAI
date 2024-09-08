17823e9b80d78310c5af3efbc66a7ebf04500eb4
jinny520 <50129077+jinny520@users.noreply.github.com>
2024-09-08 22:41:55 +0900

    Update 02_강의자료.py

--- pages/02_강의자료.py
+++ pages/02_강의자료.py
@@ -10,13 +10,13 @@ pdf_file_path_lecture = os.path.join(os.path.dirname(__file__), "Note.pdf")
 if os.path.exists(pdf_file_path_lecture):
     # 강의자료 PDF 파일 다운로드 버튼
     with open(pdf_file_path_lecture, "rb") as file:
+        st.write("버튼을 클릭하여 강의자료 PDF 파일을 다운로드하세요.")
         btn_lecture = st.download_button(
             label="분류와 회귀 수업자료",
             data=file,
             file_name="Note.pdf",
             mime="application/pdf"
         )
-    st.write("버튼을 클릭하여 강의자료 PDF 파일을 다운로드하세요.")
 else:
     st.error("강의자료 파일을 찾을 수 없습니다. 파일 경로와 이름을 확인해주세요.")
 
